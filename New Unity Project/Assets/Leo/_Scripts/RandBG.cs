0bbc7d53cb558616b01e173e726e26e0fad72532
72gsir <72gsir@gmail.com>
2017-12-02 18:44:14 +0300

    New proj

---
+++ New Unity Project/Assets/Leo/_Scripts/RandBG.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandBG : MonoBehaviour {
+
+	public Material[] materials;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent <Skybox> ().material = materials [Random.Range(0, materials.Length)];
+	}
+
+}
